71790e89e6e14bb68ac85a4493e5de23fbb0e895
Ovidiu Bokar <ovidiu.bokar@timeinc.comm>
2016-11-14 21:04:22 +0000

    testing typescript

--- dist/social-media.js
+++ dist/social-media.js
@@ -1,13 +1,1 @@
-var Greeter = (function () {
-    function Greeter(greeting) {
-        this.greeting = greeting;
-    }
-    Greeter.prototype.greet = function () {
-        return "<h1>" + this.greeting + "</h1>";
-    };
-    return Greeter;
-}());
-;
-var greeter = new Greeter("Hello, world!");
-document.body.innerHTML = greeter.greet();
 //# sourceMappingURL=social-media.js.map
