bd52f6c166165bfaac915f42501b94fa06fcd5cc
Ovidiu Bokar <ovidiu.bokar@timeinc.comm>
2016-11-14 17:42:13 +0000

    implemented typescript & started restructure code
    added basic travis.yml
    
    remove node versions from travis ci
    
    added npm install in travis
    
    remove before script install travis
    
    Update travis nvm version 6.1
    
    Added karma to package.json
    
    remove duplicated scripts tags in package.json
    
    start karma
    
    update package.json
    
    travis status
    
    testing typescript
    
    typescript works
    
    updated tsc to compile in dist folder
    
    added documentation to class, create anchors with links
    
    update documentation and finished icons
    
    implemented minify in gulp
    
    test config
    
    updated server command
    
    testing
    
    testing
    
    test
    
    testing with codecept
    
    test
    
    test
    
    test mocha
    
    updated tests & build dist in window
    
    updated tests
    
    updated git ignore
    
    update composer
    
    updated readme

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 [![bitHound Dependencies](https://www.bithound.io/github/bovidiu/js-sharing-bootstrap/badges/dependencies.svg)](https://www.bithound.io/github/bovidiu/js-sharing-bootstrap/v1.1/dependencies/npm)
 [![bitHound Overall Score](https://www.bithound.io/github/bovidiu/js-sharing-bootstrap/badges/score.svg)](https://www.bithound.io/github/bovidiu/js-sharing-bootstrap)
-
+[![Build Status](https://travis-ci.org/bovidiu/js-sharing-buttons.svg?branch=v1.4-dev)](https://travis-ci.org/bovidiu/js-sharing-buttons)
 #Social Media Sharing Buttons
 Social media buttons to share your page to social media; already configured to use font-awesome icons.
 
@@ -10,19 +10,17 @@ The current library of social media list includes:
 * LinkedIn
 * Pinterest
 
-#Next update
-In the next version I will add options to customise the icons displayable.
-ie: as a user i will only want to display facebook and twitter in the footer of the page, however in the header I would like to have all icons.
+#Install
+You can use composer to install it `composer require bovidiu/js-sharing-bootstrap` 
+
+Or implement it manually by adding the script in the footer like:
+
+```$xslt                                    
+<script src="/path/to/js/SocialMedia.min.js"></script>
+```
 
 
-#How to use
-Add the library into your page footer and make sure that on the pages exists an element with a class named: `socialMedia` .
+#Configure
+By default it will look for `socialMedia` class and will try to render Facebook and Twitter icons
 
-#Log
-
-##v1.1
-It's a remake of Master branch and v1.0 where it was configured with 2 libraries one of which was a jQuery approach.
-
-This new version is build only into JavaScript and the library has 1.5kb compressed. Also I have added a composer.json to be easier to install.
- 
 
---
+++ ts/SocialMedia.ts
@@ -0,0 +1,148 @@
+import { SocialMediaInterface } from './SocialMediaInterface';
+export class SocialMedia implements SocialMediaInterface {
+  config: any;
+  userConfig: any;
+
+  /**
+   * Class constructor
+   * Builds default configurations and sets any custom configuration if the parameter is pass
+   * @param user
+   */
+  constructor(user = null) {
+    this.config = {
+      defaultTarget : 'socialMedia',
+      icon : {
+        facebook : {
+          name: 'facebook',
+          active : true,
+          url : 'http://www.facebook.com/sharer.php?u='
+        },
+        twitter : {
+          name: 'twitter',
+          active : true,
+          url : 'http://twitter.com/share?url='
+        },
+        pinterest : {
+          name: 'pinterest',
+          active : false,
+          url : 'http://www.linkedin.com/shareArticle?mini=true&url='
+        },
+        linkedin : {
+          name: 'linkedin',
+          active : false,
+          url : 'http://pinterest.com/pin/create/button/?url='
+        },
+      }
+    };
+    this.userSetup(user);
+  }
+
+  /**
+   * Merge default config with user config and return only one object config
+   * @returns {any}
+   */
+  getConfig(): any {
+    let defaultConfig = this.config;
+    let user          = this.userConfig;
+    let allConfig     = this.mergeRecursive(defaultConfig, user);
+    return allConfig;
+  }
+
+  /**
+   * Recursivly merge two pbjects into one
+   * @param obj1
+   * @param obj2
+   * @returns {any}
+   */
+  mergeRecursive(obj1, obj2): any {
+    for (var p in obj2) {
+      try {
+        if (obj2[p].constructor == Object) {
+          obj1[p] = this.mergeRecursive(obj1[p], obj2[p]);
+        } else {
+          obj1[p] = obj2[p];
+        }
+      } catch (e) {
+        obj1[p] = obj2[p];
+      }
+    }
+    return obj1;
+  }
+
+  /**
+   * Get configuration per section
+   * @param elem
+   * @returns {any}
+   */
+  getConfigElement(elem): any {
+    let currentConfig = this.getConfig();
+    return currentConfig[elem];
+  }
+
+  /**
+   * Set user configurations
+   * @param user
+   * @returns {any}
+   */
+  userSetup(user): any {
+    return this.userConfig = user;
+  }
+
+  /**
+   * Initialise creation of links
+   */
+  init(): any {
+    let currentTag    = this.getConfigElement('defaultTarget');
+    if (currentTag !== 'undefined') {
+      let availableTags = document.querySelectorAll('.' + currentTag);
+      if (availableTags.length) {
+        for (let i = 0; i < availableTags.length; i++) {
+          this.createIcons(currentTag,i)
+        }
+      }
+    }
+  }
+
+  /**
+   * get a list of active icons and build the anchor
+   * @param currentTag
+   * @param i
+   */
+  createIcons(currentTag, i): any{
+    let activeIcons = this.getActiveIcons();
+    for(let icon in activeIcons){
+      this.createAnchor(currentTag, i,activeIcons[icon].name,activeIcons[icon].url);
+    }
+  }
+
+  /**
+   *  Filter the configuration to get only the active icons
+   * @returns {Array}
+   */
+  getActiveIcons(): any {
+    let availabeTypes = this.getConfig();
+    let results       = [];
+    for (let i in availabeTypes.icon) {
+      if (availabeTypes.icon[i].active) {
+        results.push(availabeTypes.icon[i]);
+      }
+    }
+    return results;
+  }
+
+  /**
+   * Create an anchor element by passing the target element, index of target element
+   * element name (which becomes the class) and element url
+   * @param target
+   * @param i
+   * @param cls
+   * @param url
+   */
+  createAnchor(target, i, cls, url): any {
+    let availableTags = document.querySelectorAll('.' + target);
+    let elem          = document.createElement('a');
+    elem.className    = 'social-icon icon-'+cls;
+    elem.href         = url + window.location.href;
+    availableTags[i].appendChild(elem);
+  }
+}
