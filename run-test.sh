2ad027213bc80b55f53401f54eea8219fb801985
Ovidiu Bokar <ovidiu.bokar@timeinc.comm>
2016-11-24 16:47:19 +0000

    testing

--- run-test.sh
+++ run-test.sh
@@ -4,9 +4,13 @@
 echo "Building JS"
 ./node_modules/typescript/bin/tsc
 
+##copy files over
+echo "Copy scripts over"
+cp dist/SocialMedia.js test/browser/
+
 ## Run tests
 echo "Page loading time test  "
 ./node_modules/phantomjs/bin/phantomjs test/pageloader.js http://127.0.0.1:8080
 
 echo "Starting tests"
-./node_modules/phantomjs/bin/phantomjs test/test.js
+./node_modules/phantomjs/bin/phantomjs test/test.js                          ßß
--- test/test.js
+++ test/test.js
@@ -2,9 +2,8 @@ var webPage = require('webpage');
 var page = webPage.create();
 
 page.open('http://127.0.0.1:8080', function(status) {
-   console.log(status);
+   console.log("Page status:",status);
   var title = page.evaluate(function() {
-    throw new Error("Something went badly wrong!");
     return document.title;
   });
   console.log("Page Title: " + title);
