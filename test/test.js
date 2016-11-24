0c1cd1da04d3c88799f052c5570f9a3496ff6c05
Ovidiu Bokar <ovidiu.bokar@timeinc.comm>
2016-11-24 16:10:13 +0000

    testing

--- test/test.js
+++ test/test.js
@@ -4,10 +4,23 @@ var page = webPage.create();
 page.open('http://127.0.0.1:8080', function(status) {
    console.log(status);
   var title = page.evaluate(function() {
+    throw new Error("Something went badly wrong!");
     return document.title;
   });
+  console.log("Page Title: " + title);
 
-  console.log(title);
+  var facebookWrapperIcon = page.evaluate(function(){
+    var fbOnly =  document.getElementsByClassName("FacebookIconOnly");
+    if(fbOnly.length){
+      return true;
+    }else{
+      throw new Error("Something went badly wrong!");
+    }
+  });
+   console.log(facebookWrapperIcon);
+
+
+
   phantom.exit();
 
 });
