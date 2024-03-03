dd253f6e3836c917507c7acf5af1f5ab429cd84e
Ovidiu Bokar <ovidiu.bokar@gmail.com>
2024-03-03 17:32:12 +0000

    jest config

---
+++ src/SocialMedia.test.ts
@@ -0,0 +1,13 @@
+//import SocialMedia from "./SocialMedia";
+const SM = require("./SocialMedia");
+
+describe('SocialMedia initialisation', () => {
+    it('should have a default config object available', () => {
+        /*expect(SocialMedia.defaultConfig).toBe({
+            placeholder:".socialMedia",
+            version:"[VERSION]",
+            icon:{}
+        });*/
+        expect(1).toBe(1);
+    });
+});
