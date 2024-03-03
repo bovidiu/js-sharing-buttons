4679d0a22772d041a638503019e1c98c8f19cd66
Ovidiu Bokar <ovidiu.bokar@gmail.com>
2024-03-03 16:47:25 +0000

    commenting code SM.ts

--- src/SocialMedia.ts
+++ src/SocialMedia.ts
@@ -8,13 +8,20 @@ let defaultConfig:defaultConfig = {
     icon:{}
 };
 
+/**
+ * Initializing custom config.
+ *
+ * @param config
+ */
 const display = (config:defaultConfig) =>{
     let customConfig:defaultConfig = {...defaultConfig, ...config};
     if(Object.keys(customConfig).length){
         socialCall(customConfig);
     }
 }
-
+/**
+ * Default exposure to browser.
+ */
 export default window.SocialMedia={
     defaultConfig,
     display
